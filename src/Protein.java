b9a500671240f50fb470981c63b8eba3c6fd841e
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-07 16:22:05 -0500

    question
    QUESTION: automatic constructors due to inheritance? is that possible?

--- src/Protein.java
+++ src/Protein.java
@@ -4,6 +4,9 @@
 
 
 public class Protein extends Nutrient {
+
+    //QUESTION: does there exists a "contractual way" or automatic way to force an inheriting class to have the
+    //          constructor already that a parent has?
     public Protein (int grams){
         super(grams);
     }
