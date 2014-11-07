2174e7f88b4f5f8c0c1141fceac8f90c7d13a4fc
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-07 16:55:09 -0500

    invalid: interface cannot extend class. duh

--- src/Tester.java
+++ src/Tester.java
@@ -6,20 +6,5 @@ public class Tester {
 
         Protein p = new Protein(2);
         System.out.println(p.grams);
-
-        //OK, WHAT'S UP. I thought abstract classes couldn't be instantiated
-        //http://stackoverflow.com/questions/4579305/can-we-instantiate-an-abstract-class
-        //SO says ANONYMOUS classes can be instantiated
-        //when typing, my IDE asked me which method to override.
-        //therefore, i'm believing this is now an anonymous class instantiation.
-        Nutrient n = new Nutrient(3) {
-            @Override
-            public int hashCode() {
-                return super.hashCode();
-            }
-        };
-        System.out.println(n.grams);
-
-
     }
 }
