21c48144c6d1a3da5fce9a88f20ac6bb92870d71
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-09 13:54:23 -0500

    isolated FoodEaten, not using ATM

--- src/FoodEaten.java
+++ src/FoodEaten.java
@@ -1,7 +1,7 @@
 /**
  * Created by IAN on 09/11/14.
  */
-public class FoodEaten {
+public abstract class FoodEaten {
     double portion;
     //http://stackoverflow.com/questions/5250646/can-we-stop-the-run-of-a-constructor
     //Make the constructor private and expose a static factory method, that will validate and return a new instance of the object if the parameter is valid.
@@ -10,11 +10,12 @@ public class FoodEaten {
     }
 
     //factory pattern attempt
-    abstract FoodEaten create(double portion){ //error: Illegal combination of modifiers
-        //validate:
-        if (portion >= 0 || portion <= 1){
-            return new FoodEaten(portion);
-        }
-        return null;
-    }
+    abstract FoodEaten create(double portion);
+
+    //validate:
+//    if (portion >= 0 || portion <= 1){
+//        return new FoodEaten(portion);
+//    }
+//    return null;
+
 }
